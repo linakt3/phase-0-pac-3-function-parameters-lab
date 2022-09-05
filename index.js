b17e248b87f319001022c106025519ce38043649
Gloria Paulina Gonzalez Hernandez <ghpaulina3@gmail.com>
2022-09-04 22:04:25 -0600

    commit tree cf19d01a966336a1becd292eae60fbdc8b66f2d6

---
+++ index.js
@@ -0,0 +1,23 @@
+function introduction(name = Aki) {
+    console.log('Hi, my name is ${name}.');
+    const name = "Aki";
+}
+function introductionWithLanguage(name, language) {
+    const name = "Aki";
+    language = "Ember.js.";
+    console.log(Hi, my name is ${name} and I am learning to program in ${languague});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
